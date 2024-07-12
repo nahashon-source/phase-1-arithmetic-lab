59a21eef536f26c1bdfe68a200c33c6047a0d0bd
nahashon <nashonmwendwa0@gmail.com>
2024-07-12 20:56:58 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let numbers = [10, 15, 20, 5];
+let max = Math.max(...numbers);
